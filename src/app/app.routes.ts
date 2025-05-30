341063b7408ad50478f9e4a3ca5b583720995c32
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2025-05-30 12:16:57 -0300

    UPD: enhance routing with LoginComponent and NotFoundComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./core/auth/pages/login/login.component').then(m => m.LoginComponent),
+  },
+  {
+    path: '**',
+    loadComponent: () => import('./pages/not-found/not-found.component').then(m => m.NotFoundComponent),
+    title: 'Página no encontrada',
+  },
+];
