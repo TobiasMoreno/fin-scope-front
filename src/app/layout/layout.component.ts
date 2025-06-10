42b2880e673a146c4b83134ad3c28947ff0b9c3d
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2025-06-10 19:44:06 -0300

    FEAT: implements new card generic

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from "../shared/components/footer/footer.component";
+import { FooterComponent } from '../shared/components/footer/footer.component';
+import { CardComponent } from '../shared/components/card/card.component';
 
 @Component({
   selector: 'app-layout',
-  standalone: true,
-  imports: [RouterOutlet, SidebarComponent, RouterLink, FooterComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    RouterLink,
+    FooterComponent,
+    CardComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css'],
 })
@@ -16,4 +22,11 @@ export class LayoutComponent {
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
+  vacancy = {
+    logoCompany: 'img/logo-light.jpg',
+    nameCompany: 'Company Name',
+    rol: 'Software Engineer',
+    location: 'New York, NY',
+    creationDate: '2021-01-01',
+  };
 }
